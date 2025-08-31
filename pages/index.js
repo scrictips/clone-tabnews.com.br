a8cbb11e80d4fd03b722ed61995a51cb3657fc6c
Scrictips <scrictips@gmail.com>
2025-08-31 17:45:21 -0300

    Adiciona a risadinha

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Thaissa amo você. Se você me ama, dá uma risadinha!</h1>
+    return <h1>Thaissa amo você. Se você me ama, dá uma risadinha! rsrsrs</h1>
 }
 
 export default Home;
