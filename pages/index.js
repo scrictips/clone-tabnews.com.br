88ce5a14cebb2a8062139084187a7e05c75477ab
Scrictips <scrictips@gmail.com>
2025-08-31 17:50:59 -0300

    Adiciona meu nome e uma coisinha a mais

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Thaissa amo você. Se você me ama, dá uma risadinha! rsrsrs</h1>
+function Home() {
+    return <body>
+                <h1>Thaissa amo você. Se você me ama, dá uma risadinha! rsrsrs</h1>
+                <p>Meu nome não é Ronaldo</p>
+                <br/>
+                <p> Cada dia aprendendo mais no curso</p>
+            </body>
 }
 
 export default Home;
