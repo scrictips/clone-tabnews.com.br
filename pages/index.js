680b6c178029c10bcc6601075647ba2b3b348e89
Scrictips <scrictips@gmail.com>
2025-09-03 22:31:06 -0300

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 function Home() {
-    return <body>
-                <h1>Thaissa amo você. Se você me ama, dá uma risadinha! rsrsrs</h1>
-                <p>Meu nome não é Ronaldo</p>
-                <br/>
-                <p> Cada dia aprendendo mais no curso</p>
-            </body>
+  return (
+    <body>
+      <h1>Thaissa amo você. Se você me ama, dá uma risadinha! rsrsrs</h1>
+      <p>Meu nome não é Ronaldo</p>
+      <br />
+      <p> Cada dia aprendendo mais no curso</p>
+    </body>
+  );
 }
 
 export default Home;
